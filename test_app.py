0df20b09ea2ce989e3a5ace3f0adbc811a36ebcf
Timur Gainutdinov <timurgainutdinov@bk.ru>
2024-04-03 13:51:43 +0500

    edited test_app.py

--- test_app.py
+++ test_app.py
@@ -1,5 +1,5 @@
 from streamlit.testing.v1 import AppTest
-
+import time
 at = AppTest.from_file("image_classification_streamlit.py", default_timeout=1000).run()
 
 
@@ -9,6 +9,7 @@ def test_correct_url():
     """
     at.text_input[0].set_value("https://goo.su/TDcn").run()
     at.button[0].click().run()
+    time.sleep(20)
     assert "Результаты распознавания: :rainbow[tabby, tabby cat]" in at.markdown[0].value
 
 
