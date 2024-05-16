4232530a9228f6a4acf4c147377dd24b3ca666a6
Bulrush3 <mike10.09.2000@yandex.ru>
2024-05-16 12:59:01 +0500

    code improvement #2

--- image_classification.py
+++ image_classification.py
@@ -4,6 +4,7 @@ import requests
 from PIL import Image
 from transformers import ViTForImageClassification, ViTImageProcessor
 
+# URL изображения
 url = (
     "https://img.freepik.com/premium-photo/"
     "a-house-on-a-mountain-with-a-mountain-"
@@ -12,13 +13,20 @@ url = (
 # Используем контекстный менеджер для открытия изображения
 with Image.open(requests.get(url, stream=True).raw) as image:
 
+    # Инициализация процессора и модели
     processor = (ViTImageProcessor
                 .from_pretrained("google/vit-base-patch16-224"))
     model = (ViTForImageClassification
             .from_pretrained("google/vit-base-patch16-224"))
 
+    # Подготовка изображения для модели
     inputs = processor(images=image, return_tensors="pt")
+
+    # Получение предсказания от модели
     outputs = model(**inputs)
     logits = outputs.logits
     predicted_class_idx = logits.argmax(-1).item()
-    print("Predicted class:", model.config.id2label[predicted_class_idx])
+    
+    # Вывод предсказанного класса
+    predicted_class = model.config.id2label[predicted_class_idx]
+    print(f"Predicted class: {predicted_class}")
